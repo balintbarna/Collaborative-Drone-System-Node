f4e813aed48c61bcea88a9a0f84bcf22be3b1b57
Balint Kovari <balintmaci@gmail.com>
2020-05-22 03:09:28 +0200

    modify launch files and scripts to better tool development

--- startupScripts/setup.sh
+++ startupScripts/setup.sh
@@ -40,7 +40,3 @@ cd $FIRMDIR
 # Needed environment for running SITL
 source Tools/setup_gazebo.bash $(pwd) $(pwd)/build/posix_sitl_default
 export ROS_PACKAGE_PATH=$ROS_PACKAGE_PATH:$(pwd):$(pwd)/Tools/sitl_gazebo
-
-# # launch basic PX4 SITL
-roslaunch power-line-inspection main.launch
-# roslaunch px4 
