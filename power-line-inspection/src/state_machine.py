ad4f2d2af8b43bfe00167ab9551d11c8ecd6140a
Balint Kovari <balintmaci@gmail.com>
2020-05-23 20:00:10 +0200

    added align pos part
    also detailed pid constructors with limit values
    also align pos and yaw only works when fresh lidar data is available

--- power-line-inspection/src/state_machine.py
+++ power-line-inspection/src/state_machine.py
@@ -1,9 +1,12 @@
 from enum import Enum
+from math import sqrt, cos, sin
+import numpy as np
 
 from geometry_msgs.msg import TwistStamped, PoseStamped, PoseWithCovarianceStamped, Vector3, Vector3Stamped, Point, Quaternion, Pose
-from message_tools import orientation_to_yaw
 import rospy
 from simple_pid import PID
+
+from message_tools import orientation_to_yaw, point_to_arr, arr_to_point
 
 class StateMachine():
     class States(Enum):
@@ -24,7 +27,25 @@ class StateMachine():
         self.pose_error = Pose()
 
         self._pose_error_sub = rospy.Subscriber("/wire_displacement", PoseStamped, self._pose_error_callback)
-        self.yaw_pid = PID()
+        pid_limit = 0.05 # about 3 degrees
+        self.yaw_pid = PID(
+            Kp=0.1,
+            Ki=0.0,
+            Kd=0.0,
+            setpoint=0,
+            sample_time=None,
+            output_limits=(-pid_limit, pid_limit))
+
+        pid_limit = 0.05
+        self.pos_pid = PID(
+            Kp=0.1,
+            Ki=0.0,
+            Kd=0.0,
+            setpoint=0,
+            sample_time=None,
+            output_limits=(-pid_limit, pid_limit))
+        
+        self.max_align_error = 0.05
     
     def _pose_error_callback(self, topic = PoseStamped()):
         self.pose_error = topic.pose
@@ -53,31 +74,70 @@ class StateMachine():
             self._mav1.set_target_pos(target)
             self._mav2.set_target_pos(target)
             self.set_current_state(self.States.WAITING_TO_ARRIVE)
-            self.set_next_state(self.States.CLOSE_TO_WIRE)
-
-        elif cur == self.States.CLOSE_TO_WIRE:
-            target = Point(-30, 30, 10)
-            self._mav1.set_target_pos(target)
-            self._mav2.set_target_pos(target)
-            self.set_current_state(self.States.WAITING_TO_ARRIVE)
+            # self.set_next_state(self.States.CLOSE_TO_WIRE)
             self.set_next_state(self.States.UNDER_WIRE)
 
+        # elif cur == self.States.CLOSE_TO_WIRE:
+        #     target = Point(-35, 30, 12)
+        #     self._mav1.set_target_pos(target)
+        #     self._mav2.set_target_pos(target)
+        #     self.set_current_state(self.States.WAITING_TO_ARRIVE)
+        #     self.set_next_state(self.States.UNDER_WIRE)
+
         elif cur == self.States.UNDER_WIRE:
-            target = Point(-35.5, 30, 13.5)
+            target = Point(-35.2, 30, 11)
             self._mav1.set_target_pos(target)
             self._mav2.set_target_pos(target)
             self.set_current_state(self.States.WAITING_TO_ARRIVE)
             self.set_next_state(self.States.ALIGN_YAW)
+            self.pose_error = None
             # self.set_next_state(self.States.IDLE)
 
         elif cur == self.States.ALIGN_YAW:
+            if self.pose_error == None:
+                return
+            if not self._mav1.has_arrived():
+                return
             angle_error = orientation_to_yaw(self.pose_error.orientation)
+            self.pose_error = None
             print("angle error "+str(angle_error))
+            if abs(angle_error) < self.max_align_error:
+                self.set_current_state(self.States.ALIGN_POS)
+                print("YAW ALIGNED")
+                self.max_align_error -= 0.01
+                return
             pid_out = self.yaw_pid(angle_error)
             current_yaw = orientation_to_yaw(self._mav1.current_pose.pose.orientation)
             target_yaw = current_yaw + pid_out
             self._mav1.set_target_yaw(target_yaw)
-            if angle_error < 0.1:
-                self.set_current_state(self.States.IDLE)
-                print("YAW ALIGNED")
+            self._mav2.set_target_yaw(target_yaw)
+        
+        elif cur == self.States.ALIGN_POS:
+            if self.pose_error == None:
+                return
+            if not self._mav1.has_arrived():
+                return
+            err = point_to_arr(self.pose_error.position)
+            self.pose_error = None
+            magnitude = np.linalg.norm(err)
+            print("pos error "+str(magnitude))
+            if magnitude < self.max_align_error*20:
+                self.set_current_state(self.States.ALIGN_YAW)
+                print("POS ALIGNED")
+                self.max_align_error -= 0.01
+                return
+            current_pos = point_to_arr(self._mav1.current_pose.pose.position)
+            yaw = orientation_to_yaw(self._mav1.current_pose.pose.orientation)
+            err[0] = cos(yaw)*err[0] + sin(yaw)*err[1]
+            err[1] = -sin(yaw)*err[0] + cos(yaw)*err[1]
+            pid_out = self.pos_pid(magnitude)
+            ratio = abs(pid_out / magnitude)
+            target = current_pos + ratio * err
+            target_point = arr_to_point(target)
+            self._mav1.set_target_pos(target_point)
+            self._mav2.set_target_pos(target_point)
+
+
+
+
 
